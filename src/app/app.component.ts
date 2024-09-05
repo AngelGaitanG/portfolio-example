594881f8d2932fd5741031af70bea147b977a00c
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-09-05 16:53:42 -0300

    Enhanced navbar with animated navigation elements on component focus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HomeComponent } from './features/home/home.component';
+import { PortfolioComponent } from './features/portfolio/portfolio.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { AboutComponent } from './features/about/about.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, PortfolioComponent, ContactComponent, PortfolioComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,42 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  activeSection: string = "";
+
   @ViewChild('expandedMenu') expandedMenu!: ElementRef;
   @ViewChild('menuBtn') menuBtn!: ElementRef;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = document.querySelectorAll('app-home, app-portfolio, app-about, app-contact');
+    let currentSection = '';
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top >= 0 && rect.top < window.innerHeight / 2) {
+        currentSection = section.tagName.toLowerCase();
+      }
+    });
+
+    this.activeSection = currentSection;
+  }
+  
+  scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+
+      history.pushState(null, '', sectionId);
+    }
+  }
 
   openMenu() {
     if (this.expandedMenu) {
